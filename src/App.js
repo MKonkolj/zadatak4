802d01e968a1e430a0e8d020f13977c18e763f11
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-02-09 21:00:33 +0100

    odrađen html i respo css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <header className='nav'>
+          <h1>Our Menu</h1>
+          <ul className='nav-list'>
+            <li className='nav-item'>All</li>
+            <li className='nav-item'>Breakfast</li>
+            <li className='nav-item'>Lunch</li>
+            <li className='nav-item'>Shakes</li>
+          </ul>
+        </header>
+        <div className="container">
+          {/* MENU ITEM
+          <div className="menu-item">
+            <img src="https://react-projects-5-menu.netlify.app/images/item-1.jpeg" alt="" />
+            <div className="menu-item-text">
+              <div className="menu-item-name-price">
+                <h4>Buttermilk Pancakes</h4>
+                <p>$15</p>
+              </div>
+              <div className="menu-item-text-desc">
+                <p>I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed</p>
+              </div>
+            </div>
+          </div>
+          MENU ITEM END */}
+        </div>
+      </main>
     </div>
   );
 }
